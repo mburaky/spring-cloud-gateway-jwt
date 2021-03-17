f4f5f87b90d680c7ed6c800907b313017b0f2913
Burak Yildirim <burak@justmop.com>
2021-03-17 07:12:33 +0000

    Api gateway implementation

--- src/main/java/com/justmop/jwtdemo/JwtDemoApplication.java
+++ src/main/java/com/justmop/jwtdemo/JwtDemoApplication.java
@@ -2,8 +2,11 @@ package com.justmop.jwtdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JwtDemoApplication {
 
 	public static void main(String[] args) {
